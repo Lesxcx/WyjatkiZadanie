4bce90077bc528df30cb4d71f63152da192f13c7
Lesxcx <szydlowski.konrad1999@gmail.com>
2023-01-25 21:06:05 +0100

    Creating a constructor for calculating the square root and adding exception handling

--- src/main/java/org/example/SquareRoot.java
+++ src/main/java/org/example/SquareRoot.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class SquareRoot {
+    double number, numberSquare;
 
+    public SquareRoot() throws InputMismatchException, IllegalArgumentException{
+        System.out.println("Enter the number you want to square: ");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            this.number = scanner.nextInt();
+            if (number < 0) {throw new IllegalArgumentException("Number cannot be negative!");}
+            numberSquare = Math.sqrt(number);
+            System.out.println(numberSquare);
+        } catch (InputMismatchException e) {
+            System.out.println("There have to be positive number!");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Number cannot be negative!");
+        }
+    }
 }
